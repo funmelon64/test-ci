cf0cd6dd28d2bcae550fb9282ddb7ac84d10fb35
Morph <from-server@fm64.me>
2025-11-07 10:45:17 +0500

    test change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return {"status": "ok", "message": "hello"}
+    return {"status": "ok", "message": "hello test"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
